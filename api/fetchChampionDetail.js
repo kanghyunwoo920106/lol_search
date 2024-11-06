2d5e38a9752e40f573db4f2253a0f9f26149cf69
hyunoo <xkaizew123@gmail.com>
2024-11-07 08:58:44 +0900

    수정

--- api/fetchChampionDetail.js
+++ api/fetchChampionDetail.js
@@ -2,14 +2,14 @@
 export default async function handler(req, res) {
     const { championId } = req.query; // Query로 넘어온 챔피언 ID
     const API_URL = `https://ddragon.leagueoflegends.com/cdn/13.20.1/data/ko_KR/champion/${championId}.json`;
-    const API_KEY = 'RGAPI-ee3ff5c3-bdb7-4b3b-8786-f3a0a823a9fe'
+    // const API_KEY = 'RGAPI-ee3ff5c3-bdb7-4b3b-8786-f3a0a823a9fe'
 
     try {
         const response = await fetch(API_URL, {
             method: "GET",
             headers: {
                 'Content-Type': 'application/json',
-                'X-Riot-Token': API_KEY
+                // 'X-Riot-Token': API_KEY
             }
         });
 
